e5aa9d7ba9a1b7d5347a4016b7bb5e66dfcec256
rheeshabh <rheeshabhkumarsingh@gmail.com>
2026-08-02 04:00:37 +0530

    two way binding

--- src/app/new-task/new-task.ts
+++ src/app/new-task/new-task.ts
@@ -1,14 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.html',
   styleUrl: './new-task.css',
 })
 export class NewTask {
   @Input() userId!: string;
+  @Output() cancel = new EventEmitter<void>()
   constructor(){}
   taskTitle: string = '';
   taskSummary: string = '';
+  taskDate: string = '';
+
+
+  onCancel() {
+    this.cancel.emit()
+  }
+
 }
